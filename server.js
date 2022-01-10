bc6310c9db72169334959e6af88dd828665bdda2
eren-ozdemir <eren2624@gmail.com>
2022-01-10 03:15:31 +0300

    v1.0

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "client", "public", "index.html")));
+console.log(path.join(__dirname, "client", "public", "index.html"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "public", "index.html"));
   console.log("hey");
@@ -212,6 +213,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log("Server started on port: ", port));
